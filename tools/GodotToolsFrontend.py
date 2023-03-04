7d7d9bf5bc08227adb26cbcf6ad24bc7d2838fe6
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2023-03-04 01:06:42 -0800

    Initial commit

---
+++ tools/GodotToolsFrontend.py
@@ -0,0 +1,8 @@
+#@Brief The goal of this file is to offer a frontend into the generators
+#offered by the Godot dev team as to avoid duplicate generator code as much as
+#possible.
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
